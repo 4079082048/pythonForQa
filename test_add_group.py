43ab2a10c7de70f7e18b557c9581899b5368bb7c
szolotova <s.zolotova@artlogics.ru>
2024-10-24 10:44:56 +0300

    readability of the code v1.0

--- test_add_group.py
+++ test_add_group.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-# import unittest, time, re
-from selenium.webdriver.firefox.webdriver import  WebDriver
-import time, unittest
+
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -17,35 +14,55 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-      #   wd.find_element_by_name("user").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.submit_group_create(wd)
+        self.return_to_group_page(wd)
+
+    def return_to_group_page(self, wd):
+        # return to group page
+        wd.find_element(By.LINK_TEXT, "group page").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+
+    def submit_group_create(self, wd):
+        # submit group create
+        wd.find_element(By.NAME, "submit").click()
+
+    def open_group_page(self, wd):
+        # open group page
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys("Group1")
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys("header")
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys("footer")
+        wd.find_element(By.NAME, "group_name").click()
+
+    def login(self, wd):
+        #  login
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
         wd.find_element(By.NAME, "user").send_keys("admin")
         wd.find_element(By.NAME, "pass").clear()
         wd.find_element(By.NAME, "pass").send_keys("secret")
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT,"groups").click()
-        wd.find_element(By.NAME, "new").click()
-        wd.find_element(By.NAME,"group_name").click()
-        wd.find_element(By.NAME,"group_name").clear()
-        wd.find_element(By.NAME,"group_name").send_keys("Group1")
-        wd.find_element(By.NAME,"group_header").clear()
-        wd.find_element(By.NAME,"group_header").send_keys("header")
-        wd.find_element(By.NAME,"group_footer").clear()
-        wd.find_element(By.NAME,"group_footer").send_keys("footer")
-        wd.find_element(By.NAME,"group_name").click()
-        wd.find_element(By.NAME,"submit").click()
-        wd.find_element(By.LINK_TEXT, "group page").click()
-        wd.find_element(By.LINK_TEXT,"Logout").click()
-        wd.find_element(By.NAME,"user").clear()
-        wd.find_element(By.NAME,"user").send_keys("admin")
-    
+
+    def open_home_page(self, wd):
+        # open home mage
+        wd.get("http://localhost/addressbook/index.php")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
+    @property
     def is_alert_present(self):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
