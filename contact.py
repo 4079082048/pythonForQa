3e4a7203399d6a7bf58ee53a59d0da64167d9aa1
szolotova <s.zolotova@artlogics.ru>
2024-10-24 18:30:51 +0300

    add new contact + contact class + refactor v 1.5

--- contact.py
+++ contact.py
@@ -1,15 +1,11 @@
 class Contact:
 
-    def __init__(self, username, password, name, firstname, lastname, company, address, mobile, email, bday, bmonth, byear):
-        self.username = username
-        self.password = password
-        self.name = name
+    def __init__(self, name, firstname, lastname, company, address, mobile, email, bday, bmonth, byear, nickname, title):
         self.firstname = firstname
         self.lastname = lastname
         self.company = company
         self.address = address
         self.mobile = mobile
-        self.email = email
-        self.bday = bday
-        self.bmonth = bmonth
-        self.byear = byear
+        self.nickname = nickname
+        self.title = title
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
@@ -16,54 +17,62 @@ class TestAddContact(unittest.TestCase):
         super().__init__(methodName)
         self.wd = None
 
+
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username= "admin", password= "secret")
-        self.create_contact(wd, firstname = "Sofia", lastname = "Zolotova", company = "ailet", address= "Tula", mobile= "89096309913", nickname= "swallow", tittle= "mem")
+        self.create_contact(wd, Contact(firstname = "Sofia", lastname = "Zolotova", company = "ailet", address= "Tula", mobile= "89096309913", nickname= "swallow", title= "mem"))
         self.check_and_logout(wd)
+
+
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username= "admin", password= "secret")
+        self.create_contact(wd, Contact(firstname = "", lastname = "", company = "", address= "", mobile= "", nickname= "", title= ""))
+        self.check_and_logout(wd)
+
 
     def check_and_logout(self, wd):
         # return to main page, logout
         wd.get("http://localhost/addressbook/index.php")
         wd.find_element(By.LINK_TEXT, "Logout").click()
 
-    def create_contact(self, wd, firstname, lastname, company, address, mobile, nickname, tittle):
+    def create_contact(self, wd, contact):
         # create contact
         wd.find_element(By.LINK_TEXT, "add new").click()
         wd.get("http://localhost/addressbook/edit.php")
         wd.find_element(By.NAME, "firstname").click()
         wd.find_element(By.NAME, "firstname").clear()
-        wd.find_element(By.NAME, "firstname").send_keys(firstname)
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
         wd.find_element(By.NAME, "lastname").clear()
-        wd.find_element(By.NAME, "lastname").send_keys(lastname)
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
         wd.find_element(By.NAME, "address").click()
         wd.find_element(By.NAME, "company").click()
         wd.find_element(By.NAME, "company").clear()
-        wd.find_element(By.NAME, "company").send_keys(company)
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
         wd.find_element(By.NAME, "address").click()
         wd.find_element(By.NAME, "address").clear()
-        wd.find_element(By.NAME, "address").send_keys(address)
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
         wd.find_element(By.NAME, "theform").click()
         wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").click()
-        wd.find_element(By.NAME, "mobile").clear()
-        wd.find_element(By.NAME, "mobile").send_keys(mobile)
+        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile)
         wd.find_element(By.NAME, "nickname").click()
         wd.find_element(By.NAME, "nickname").clear()
-        wd.find_element(By.NAME, "nickname").send_keys(nickname)
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
         wd.find_element(By.NAME, "title").click()
         wd.find_element(By.NAME, "title").clear()
-        wd.find_element(By.NAME, "title").send_keys(tittle)
+        wd.find_element(By.NAME, "title").send_keys(contact.tittle)
         wd.find_element(By.XPATH, "//div[@id='content']/form/input[20]").click()
 
     def login(self, wd, username, password):
         # login
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username())
+        wd.find_element(By.NAME, "user").send_keys(username)
         wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password())
+        wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
 
