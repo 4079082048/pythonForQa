a4d686404ab8de211048c2d3709b4f03d2d49a76
szolotova <s.zolotova@artlogics.ru>
2024-10-24 13:27:14 +0300

    add new contact + contact class + refactor v 1.2

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, name, firstname, lastname, company, address, mobile, email, bday, bmonth, byear):
+        self.name = name
+        self.firstname = firstname
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
