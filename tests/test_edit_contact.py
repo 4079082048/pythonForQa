f1ce8bca470e1994fba64c272ef8253a8cd161ab
szolotova <s.zolotova@artlogics.ru>
2024-11-23 16:50:36 +0300

    Task 4.11 Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп и контактов.

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -5,21 +5,25 @@ from model.contact import Contact
 def test_edit_contact_firstname(app):
         # Get contacts list
         old_contacts = app.contact.get_contact_list()
+        contact = Contact(firstname="Sofia", lastname="Zolotova", company="ailet", address="Tula", mobile="89096309913",nickname="swallow", title="mem")
         app.open_home_page()
         if app.contact.count() == 0:
-                app.contact.create(Contact(firstname="TestCount2", lastname="TestCountZolotova2"))
-        app.contact.edit_first_contact(Contact(firstname="Ivanna"))
+                app.contact.create(contact)
+        contact.id = old_contacts[0].id
+        app.contact.edit_first_contact(contact)
         new_contacts = app.contact.get_contact_list()
         assert len(old_contacts)  == len(new_contacts)
+        old_contacts[0] = contact
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_edit_contact_lastname(app):
-        old_contacts = app.contact.get_contact_list()
-        app.open_home_page()
-        if app.contact.count() == 0:
-                app.contact.create(Contact(firstname="TestCount2", lastname="TestCountZolotova2"))
-        app.contact.edit_first_contact(Contact(lastname="Gladisheva"))
-        new_contacts = app.contact.get_contact_list()
-        assert len(old_contacts) == len(new_contacts)
+#def test_edit_contact_lastname(app):
+#        old_contacts = app.contact.get_contact_list()
+#        app.open_home_page()
+#        if app.contact.count() == 0:
+#                app.contact.create(Contact(firstname="TestCount2", lastname="TestCountZolotova2"))
+#        app.contact.edit_first_contact(Contact(lastname="Gladisheva"))
+#        new_contacts = app.contact.get_contact_list()
+#        assert len(old_contacts) == len(new_contacts)
 
 
