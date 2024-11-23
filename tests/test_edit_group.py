f7622882ff6ec77e714e92dd5316afab8e4a58db
szolotova <s.zolotova@artlogics.ru>
2024-11-23 16:44:00 +0300

    Для групп 4.8 video Сортировка списков перед сравнением групп+контактов + добавление групп+добавление контактов+редактирование групп

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -1,25 +1,31 @@
+from tokenize import group
+
 from model.group import Group
 from  fixture.application import Application
 
 
 def test_edit_group_name(app):
         old_groups = app.group.get_group_list()
-        if app.group.count_g() == 0:
-                app.group.fill_group_form(Group(name="testCreatedToModify"))
-        app.group.edit_first_group(Group(name="GroupNew"))
+        group = Group(name="testCreatedToModify")
+        group.id = old_groups[0].id #remember id of the group
+        #if app.group.count_g() == 0:
+        #        app.group.fill_group_form(group)
+        app.group.edit_first_group(group)
         new_groups = app.group.get_group_list()
         assert len(old_groups) == len(new_groups)
+        old_groups[0] = group
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
         app.session.logout()
 
 
-def test_edit_group_header(app):
-        old_groups = app.group.get_group_list()
-        if app.group.count_g() == 0:
-                app.group.fill_group_form(Group(name="testCreatedToModify"))
-        app.group.edit_first_group(Group(header="headerNew"))
-        new_groups = app.group.get_group_list()
-        assert len(old_groups) == len(new_groups)
-        app.session.logout()
+#def test_edit_group_header(app):
+#        old_groups = app.group.get_group_list()
+#        if app.group.count_g() == 0:
+#                app.group.fill_group_form(Group(name="testCreatedToModify"))
+#        app.group.edit_first_group(Group(header="headerNew"))
+#        new_groups = app.group.get_group_list()
+#        assert len(old_groups) == len(new_groups)
+#        app.session.logout()
 
 
 
