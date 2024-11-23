bf7c49ace0b505904f42f8fbc17970670d8c2ab9
szolotova <s.zolotova@artlogics.ru>
2024-11-23 15:21:25 +0300

    Для групп 4.4 созднание/изменение/удаление получаем и сравниваем списки + рабочее добавление контакта и сравнение списков + удаление контакта со сравнением списков+редактирование

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -3,17 +3,23 @@ from model.contact import Contact
 
 
 def test_edit_contact_firstname(app):
+        # Get contacts list
+        old_contacts = app.contact.get_contact_list()
         app.open_home_page()
         if app.contact.count() == 0:
                 app.contact.create(Contact(firstname="TestCount2", lastname="TestCountZolotova2"))
         app.contact.edit_first_contact(Contact(firstname="Ivanna"))
-
+        new_contacts = app.contact.get_contact_list()
+        assert len(old_contacts)  == len(new_contacts)
 
 
 def test_edit_contact_lastname(app):
+        old_contacts = app.contact.get_contact_list()
         app.open_home_page()
         if app.contact.count() == 0:
                 app.contact.create(Contact(firstname="TestCount2", lastname="TestCountZolotova2"))
         app.contact.edit_first_contact(Contact(lastname="Gladisheva"))
+        new_contacts = app.contact.get_contact_list()
+        assert len(old_contacts) == len(new_contacts)
 
 
