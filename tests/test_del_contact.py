7ae067c4d70612215924fb1eb5e1bd64b01abbdd
szolotova <s.zolotova@artlogics.ru>
2024-11-23 16:01:05 +0300

    Для групп 4.5 video Проверка равенства о удаление групп+контактов

--- tests/test_del_contact.py
+++ tests/test_del_contact.py
@@ -10,9 +10,9 @@ def test_del_first_contact(app):
     old_contacts = app.contact.get_contact_list()
     #del first contact in the list
     app.contact.del_first_contact()
-    assert len(old_contacts) - 1 == app.contact.count_contacts()
-    #get new contact list
+    # get new contact list
     new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == app.contact.count_contacts()
     #del first contact in the old list
     old_contacts[0:1] = []
     assert old_contacts == new_contacts
--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -10,5 +10,7 @@ def test_del_first_group(app):
     app.group.del_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
-
+    #del first(zero) number group
+    old_groups[0:1] = []
+    assert old_groups == new_groups
 
