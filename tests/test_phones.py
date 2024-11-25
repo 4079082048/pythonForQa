38d498583027eeef9e8bfb1579f00d0b888d4d78
szolotova <s.zolotova@artlogics.ru>
2024-11-25 12:08:53 +0300

    5.14.5 Реализованы проверки для всех полей контакта на главной странице Реализован тест, который проверяет, что для некоторого одного случайно выбранного контакта информация на главной странице (в таблице) соответствует информации, представленной в форме редактирования контакта (где задаются все его свойства),проверяются  все представленные свойства -- имя, фамилия, адрес, телефоны, адреса электронной почты.
    Для телефонов и адресов электронной почты использована технику обратных проверок.

--- tests/test_phones.py
+++ tests/test_phones.py
@@ -1,14 +1,17 @@
 import re
+from random import randrange
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    index = randrange(app.contact.count_contacts())
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
-
+#direct assert take info from  view_page and edit_page and compare it
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    index = randrange(app.contact.count_contacts())
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
