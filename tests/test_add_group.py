fded0345f2fda085739019dd09c4dfa9cc32240f
szolotova <s.zolotova@artlogics.ru>
2024-11-25 14:54:50 +0300

    5.15.1 parametrize part1

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,31 +1,26 @@
 # -*- coding: utf-8 -*-
-from sys import maxsize
+import pytest
 
-from tests.conftest import fixture
 from model.group import Group
-from  fixture.application import Application
+import pytest
+
+testdata = [
+        Group(name="Group1", header="header", footer="footer"),
+        Group(name="", header="", footer="")
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+        pass
+        #old_groups = app.group.get_group_list() #load old list
+        #app.group.fill_form(group)
+        #app.group.submit_creation()
+        #assert len(old_groups) + 1 == app.group.count_g() #assert two lists length
+        #new_groups = app.group.get_group_list()  # load new list IF only previous assert is ok
+        #old_groups.append(group)
+        #assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_group(app):
-        old_groups = app.group.get_group_list() #load old list
-        group = Group(name="Group1", header="header", footer="footer")
-        app.group.fill_form(group)
-        app.group.submit_creation()
-        assert len(old_groups) + 1 == app.group.count_g() #assert two lists length
-        new_groups = app.group.get_group_list()  # load new list IF only previous assert is ok
-        old_groups.append(group)
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-#def test_add_empty_group(app):
-#        old_groups = app.group.get_group_list()
-#        group = Group(name="", header="", footer="")
-#        app.group.fill_form(group)
-#       app.group.submit_creation()
-#        new_groups = app.group.get_group_list()
-#        assert len(old_groups) + 1 == len(new_groups)
-#        old_groups.append(group)
-#        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
