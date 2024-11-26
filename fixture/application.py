cd641d21d7f67eec4034a5f8a8d92e54bb1f6329
szolotova <s.zolotova@artlogics.ru>
2024-11-26 10:26:37 +0300

    5.16.2 реализованы base_url +добавлены опции для запуска из командной строки и среды для разных браузеров

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,7 @@
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 from fixture.contact import ContactHelper
-import time
+
 from selenium import webdriver
 
 
@@ -9,16 +9,7 @@ __author__ = 'Sofia'
 
 
 class Application:
-    def __init__(self, browser = "firefox"): #(self)
-        if browser == "firefox":
-            self.wd = webdriver.Firefox()
-        else browser == "chrome":
-            self.wd = webdriver.Chrome()
-        elif browser == "safari":
-            self.wd = webdriver.Safari()
-        else:
-            raise ValueError("Unrecognized Browser %s" % browser) #аварийное прерывание кода
-
+    def __init__(self, browser, baseUrl): #(self)
 
         if browser == "firefox":
             self.wd = webdriver.Firefox()
@@ -28,6 +19,9 @@ class Application:
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
+        #аварийное прерывание кода
+
+
         self.current_url = None
         self.wd = webdriver.Firefox() #self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(5)
@@ -35,12 +29,13 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = baseUrl
 
 
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("index.php") > 0):
-            wd.get("http://localhost/addressbook/index.php")
+            wd.get(self.base_url)
 
 
     def destroy(self):
