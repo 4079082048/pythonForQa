7a5d7c23c844d9b80870648ee713d5815507114d
szolotova <s.zolotova@artlogics.ru>
2024-11-26 11:10:39 +0300

    5.16.2 реализованы base_url +добавлены опции для запуска из командной строки и среды для разных браузеров

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,4 +32,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
