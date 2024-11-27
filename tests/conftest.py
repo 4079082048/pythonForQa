3c1fe4d03edecfc01c9739dc55b9e6384253ae17
szolotova <s.zolotova@artlogics.ru>
2024-11-27 13:31:02 +0300

    6.18.3 Для групп генерация тестовых данных

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import json
 
 from setuptools.command.setopt import config_file
-
+import importlib
 from fixture.application import Application
 import pytest
 import os.path
+
+from generator.group import testdata
 
 fixture = None
 target = None
@@ -39,3 +41,12 @@ def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
 
+
+def pytest_generate_tests(metafunc): #получить инфо о тест функции
+    for fixture in metafunc.fixturenames: #пробегаем по всем параметрам
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_module(module):
+    return importlib.import_module("data.%" % module).testdata #после импорта взять из модуля тестдата
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from tokenize import group
+
 from model.group import Group
 import pytest
-from data.add_group import constant as testdata
+from data.groups import constant as testdata
 
 
 #testdata = [
@@ -12,19 +14,28 @@ from data.add_group import constant as testdata
 #]
 
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+#@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+#def test_add_group(app, group):
+#        old_groups = app.group.get_group_list() #load old list
+#        app.group.fill_form(group)
+#        app.group.submit_creation()
+#        assert len(old_groups) + 1 == app.group.count_g() #assert two lists length
+#        new_groups = app.group.get_group_list()  # load new list IF only previous assert is ok
+#        old_groups.append(group)
+#        print("Old groups:", old_groups)
+#        print("New groups:", new_groups)
+#        print("Group being added:", group)
+#        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+def test_add_group(app, data_groups): #загружать тест данные из пакета data
+        group = data_groups
         old_groups = app.group.get_group_list() #load old list
         app.group.fill_form(group)
         app.group.submit_creation()
         assert len(old_groups) + 1 == app.group.count_g() #assert two lists length
         new_groups = app.group.get_group_list()  # load new list IF only previous assert is ok
         old_groups.append(group)
-        print("Old groups:", old_groups)
-        print("New groups:", new_groups)
-        print("Group being added:", group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
 
 
 
