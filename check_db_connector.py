55c926a1270864733eb0bf8aebf4175a761ac5ad
szolotova <s.zolotova@artlogics.ru>
2024-12-04 13:41:38 +0300

    7.22.2 Отношения между объектами Получаем список контактов без группы

--- check_db_connector.py
+++ check_db_connector.py
@@ -1,5 +1,7 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture #from fixture.db import DbFixture
+from model.group import Group
+from model.contact import Contact
 
 # было connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 # создаем объект типа db
@@ -13,14 +15,20 @@ db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 #finally:
 #    pass
 
+#try:
+#  l = db.get_contact_list() #получаем списко групп, сохраняем в переменную
+#  for item in l:
+#        print(item)#выводим список
+# print(len(l)) #выводим длину списка
+#finally:
+#    pass
+
+
 try:
-  l = db.get_contact_list() #получаем списко групп, сохраняем в переменную
+  l = db.get_contacts_not_in_group(Group(id="216")) #получаем списко групп, сохраняем в переменную
   for item in l:
         print(item)#выводим список
   print(len(l)) #выводим длину списка
 finally:
     pass
-
-
-
 
--- fixture/orm.py
+++ fixture/orm.py
@@ -17,6 +17,8 @@ class ORMFixture:
         name =Optional(str, column='group_name')
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
+        contacts = Set(lambda:ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True) #вхождение в множество и связующая таблица
+
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -24,9 +26,10 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda :ORMFixture.ORMGroup,table="address_in_groups", column="group_id", reverse="contacts", lazy=True )#подгружаем данные только при обращении = lazy
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql',host=host, database=name, user=user, password=password, conv=decoders) #сделали привязку
+        self.db.bind('mysql',host=host, database=name, user=user, password=password) #сделали привязку , conv=decoders
         self.db.generate_mapping() #сопоставляются
         sql_debug(True)
 
@@ -47,3 +50,14 @@ class ORMFixture:
         def convert(contact):
             return Contact(id=str(contact.id), firstname= contact.lastname, lastname= contact.lastname)
         return list(map(convert, contacts))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select (g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contatcs)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(
+            select (c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
