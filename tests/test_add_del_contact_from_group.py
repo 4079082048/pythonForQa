188c85f7347e0df9bb8964cc008188c475c3a95a
szolotova <s.zolotova@artlogics.ru>
2024-12-08 19:11:50 +0300

    7.22.2

--- tests/test_add_del_contact_from_group.py
+++ tests/test_add_del_contact_from_group.py
@@ -1,3 +1,5 @@
+from tokenize import group
+
 import pytest
 import random
 from random import randrange
@@ -6,15 +8,20 @@ from fixture.contact import Contact
 from fixture.group import Group
 
 
-#@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact_to_group(app, orm, data_contacts, check_ui):
-    if len(orm.contact.count()) == 0:
+
+def test_add_contact_to_group(app, orm):
+    if len(app.contact.count()) == 0:
         app.contact.create(Contact(firstname="FisrtNameAny", lastname="LastNameAny"))
-    if len(orm.get_group_list()) == 0:
+    if len(app.get_group_list()) == 0:
         app.group.fill_group_form(Group(name = "GroupAny"))
-    db_group_list = orm.get_group_list()
-    random_index = random.randrange(len(db_group_list)) #Get random group index
-    w_g_contacts = orm.get_contacts_not_in_group()
-    no_g_contacts = orm.get_contacts_not_in_group()
-    app.contact.add_contact_to_group(contact)
+    db_group_list = orm.get_group_list() #Get groups list from db
+    random_g_id = random.randrange(len(db_group_list)) #Get random group index
+    group = db_group_list[random_g_id]
+   # w_g_contacts = orm.get_contacts_not_in_group(group)
+    no_g_contacts = orm.get_contacts_not_in_group(group) #Get contacts without group from DB
+    #random_contact = random.choice(no_g_contacts) #Get random index from the list
+    random_id = random.randrange(len(no_g_contacts)) #Get random index with randrange
+    r_contact = no_g_contacts[random_id] #Get random contact from index
+    r_contact_id = r_contact.id #Get random contact id
+    app.contact.add_contact_to_group(r_contact_id.id, group.random_g_id)
     #assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
