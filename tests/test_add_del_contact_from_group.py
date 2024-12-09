a247473294424028080888957e39bb159bcf7bf3
szolotova <s.zolotova@artlogics.ru>
2024-12-09 18:44:32 +0300

    7.22.3

--- tests/test_add_del_contact_from_group.py
+++ tests/test_add_del_contact_from_group.py
@@ -10,16 +10,14 @@ from fixture.group import Group
 
 
 def test_add_contact_to_group(app, orm):
-    if len(app.contact.count()) == 0:
+    if app.contact.count_contacts() == 0:
         app.contact.create(Contact(firstname="FisrtNameAny", lastname="LastNameAny"))
-    if len(app.get_group_list()) == 0:
+    if app.group.count_g() == 0:
         app.group.fill_group_form(Group(name = "GroupAny"))
     db_group_list = orm.get_group_list() #Get groups list from db
     random_g_id = random.randrange(len(db_group_list)) #Get random group index
     group = db_group_list[random_g_id]
-   # w_g_contacts = orm.get_contacts_not_in_group(group)
     no_g_contacts = orm.get_contacts_not_in_group(group) #Get contacts without group from DB
-    #random_contact = random.choice(no_g_contacts) #Get random index from the list
     random_id = random.randrange(len(no_g_contacts)) #Get random index with randrange
     r_contact = no_g_contacts[random_id] #Get random contact from index
     r_contact_id = r_contact.id #Get random contact id
