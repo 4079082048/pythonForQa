41b006cd199b082b27bcb00ac58710c8759e8a8d
szolotova <s.zolotova@artlogics.ru>
2024-12-10 16:23:09 +0300

    7.22.9 добавили контакт в группу

--- tests/test_add_del_contact_from_group.py
+++ tests/test_add_del_contact_from_group.py
@@ -22,8 +22,12 @@ def test_add_contact_to_group(app, orm):
     no_g_contacts = orm.get_contacts_not_in_group(group) #Get contacts without group from DB
     random_id = random.randrange(len(no_g_contacts)) #Get random index in randrange
     r_contact = no_g_contacts[random_id] #Get random contact from index
-    #r_contact_id = r_contact.id #Get random contact id
-
     app.contact.add_contact_to_group(r_contact.id, group.id)
 
-    #assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+def test_del_contact_from_group(app, orm):
+    if app.contact.count_contacts() == 0:
+        app.contact.create(Contact(firstname="FisrtNameAny", lastname="LastNameAny"))
+    if app.group.count_g() == 0:
+        app.group.fill_group_form(Group(name = "GroupAny"))
+
+    app.contact.del_contact_from_group(r_contact.id, group.id)
