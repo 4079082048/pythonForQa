b23a21480d4783e613662e2a88caf28917a468c6
szolotova <s.zolotova@artlogics.ru>
2024-12-26 19:42:15 +0300

    27-7 bdd add+delete с ошибками про Step definition is not found: Given "a group with name2, header2 and footer2".

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,3 @@
-from lib2to3.pgen2 import driver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
